6c0fc3f9c71129c1c2108e094720b76cd2c89163
TimM32 <timmaupin@live.com>
2023-03-20 13:28:33 -0500

    add store objects and names

---
+++ js/app.js
@@ -0,0 +1,23 @@
+'use strict'
+console.log('js connected')
+
+
+let storeOne = {
+    storeName: 'Seattle'
+}
+
+let storeTwo = {
+    storeName: 'Toyko'
+}
+
+let storeThree = {
+    storeName: 'Dubai'
+}
+
+let storeFour = {
+    storeName: 'Paris'
+}
+
+let storeFive = {
+    storeName: 'Lima'
+}
