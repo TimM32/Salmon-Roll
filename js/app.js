974705ef991d2f08b617183f303341522cd916b2
TimM32 <timmaupin@live.com>
2023-03-20 13:41:57 -0500

    hourly total

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@ console.log('js connected')
 
 let storeOne = {
     storeName: 'Seattle'
-    businHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'pm' ]
+    hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm']
+    hourlyTotal: []
 }
 
 let storeTwo = {
